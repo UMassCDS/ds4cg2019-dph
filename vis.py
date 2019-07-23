5cfe8fec573e03e72249e2c02e80d8cedd4a6f48
Pracheta B A <pboddavarama@umass.edu>
2019-07-23 14:47:03 -0400

    visualization

--- vis.py
+++ vis.py
@@ -2,7 +2,23 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-data = pd.read_csv('output/pca_1_std.csv',index_col=0)
-print(data.columns)
-# print(data.head())
-# plt.bar(x = data.index, height=data[1])
+def pca_std():
+	data = pd.read_csv('output/pca_1_std.csv',names=['Town','Health_Score'])
+	data.set_index('Town',inplace=True)
+	t_3 = data.iloc[:3]
+	b_3 = data.iloc[-3:]
+	v_1 = pd.concat([t_3,b_3])
+	plt.bar(x=v_1.index,height=v_1['Health_Score'])
+	plt.show()
+
+def pca_mn():
+	data = pd.read_csv('output/pca_1_mn.csv',names=['Town','Health_Score'])
+	data.set_index('Town',inplace=True)
+	t_3 = data.iloc[:3]
+	b_3 = data.iloc[-3:]
+	v_1 = pd.concat([t_3,b_3])
+	plt.bar(x=v_1.index,height=v_1['Health_Score'])
+	plt.show()
+
+pca_std()
+pca_mn()

361fe26f0fef4dd6d9216e1a3cbf7abd5d9ee52d
taurigemma96 <taaurigemma@gmail.com>
2019-07-25 16:32:10 -0400

    Added Domains to PCA.py and generated clean output

--- pca.py
+++ pca.py
@@ -48,7 +48,7 @@ class HealthScores():
     '''
     Get healthscores for all towns
     '''
-    def __init__(self, cols_filepath, data_filepath, pca_filepath, loadings_filepath=None):
+    def __init__(self, cols_filepath, data_filepath, pca_filepath, loadings_filepath=None, domain_filepath=None, corrmat_filepath = None):
         '''
         Initialize variables
         '''
@@ -56,7 +56,36 @@ class HealthScores():
         self.data = data_filepath
         self.output = pca_filepath
         self.loadings_file = loadings_filepath
+        self.domain_file = domain_filepath
+        self.corrmat_file = corrmat_filepath
 
+        self.domains = {'built_environment':['no_vehicle_avail_%', 'comm_car_%', 'comm_carpool_%', 'comm_bus_%', 'comm_walk_%',\
+                    'comm_cycle_%', 'comm_taxi_%', 'comm_wfh_%','tobbaco_retailers_2019_%', 'liquor_per1000', 'supermarket_per1000', 
+                    'food_est_per1000'], 
+                    'community_context':['race_Wh_%', 'race_Afam_%', 'race_Alaska_%', 'race_Asian_%', 'race_Hawaii_%', \
+                    'race_Other_%', 'race_Two_%', 'race_Hisp_%', 'age_lt18_%', 'young_females_%', 'median_age', 'lt5years_%', 
+                    'population_density', 'rural_population_%', 'english_verywell_%', 'print_media_circ_%'],
+                    'economy':['below_poverty_level_%', 'debt_pervaluation_%', 
+                    'households_recfoodstamps_%', 'snap_%', 'median_household_income_USD', 'income_capita_USD', 'gini_index', 'median_earnings_retail_USD', 'poverty_child_%'],
+                    'education':['exp_perpupil_USD', 'preschool_%', 'students_disabilities_%', 'students_highneeds_%', 'student_teacher_ratio', 
+                    'libraries_%','exemplary_%', 'proficient_%', 'need_improve_%', 'unsatis_%', 'read_write', 'math', 'dropout_%', 'college_%', 
+                    'avg_ela', 'avg_math','less_than_highschool_%', 'highschool_grad_%', 'associates_degree_%', 'bachelors_degree_%', 
+                    'public_school_%', 'private_school_%','lep_%', 'students_econodisadv_%'],
+                    'employment':['unemployment_rate', 'labor_force_rate', 'emp_rate', 'emp_business_%', 'emp_computer_%', 'emp_legal_%', 
+                    'emp_healthcare_%', 'emp_healthsupport_%', 'emp_protective_%', 'emp_foodprepare_%', 'emp_clean_%', 'emp_personalcare_%', 
+                    'emp_sales_%', 'emp_admin_%', 'emp_farm_%', 'emp_construct_%', 'emp_repair_%', 'emp_production_%', 'emp_transport_%', 
+                    'emp_material_%', 'emp_pop_ratio_wh_only', 'emp_pop_ratio_hisp', 'employed_male_%', 'employed_female_%'],
+                    'healthcare_access':['licensed_nurses_per1000', 'dentists_per1000', 'physician_assisted_%', 'pa_provider_density_per10000', 
+                    'physicians_per1000', 'pcp_per1000', 'psychiatrist_per1000', 'psychologist_per1000', 'pharmacist_per1000', 
+                    'registerednurses_per1000', 'advancedprn_per1000', 'health_insurance_coverage_%'],
+                    'housing':['house_noplumbing_%', 'house_nokitchen_%', 'house_lt1939_%', 'house_1940to1999_%', 'house_2000to2014_%',
+                    'median_owner_home_USD', 'median_sale_homes_USD', 'house_income_gt30_USD_%', 'gross_rent_%', 'property_val_USD', 
+                    'costas%income_wmgage_lt20%_%', 'costas%income_wmgage_20to25%_%', 'costas%income_wmgage_25to30%_%', 'costas%income_wmgage_30to35%_%',
+                    'costas%income_wmgage_gt35%_%', 'costas%income_womgage_lt10%_%', 'costas%income_womgage_10to15%_%', 'costas%income_womgage_15to20%_%', 
+                    'costas%income_womgage_20to25%_%', 'costas%income_womgage_25to30%_%', 'costas%income_womgage_30to35%_%','costas%income_womgage_gt35%_%',
+                    'homeless_Shelters_per_1000', 'moved_lastyear_%', 'renter_occupied_%', 'vacant_rentals_%', 'owner_homes_%', 'occ_lt0.5_%', 
+                    'occ_0.5to1.0_%', 'occ_1.01to1.5_%', 'occ_1.51to2.0_%', 'occ_gt2.01_%'],
+                    'violence':['crimes_against_persons_%', 'crimes_against_property_%', 'crimes_against_society_%']}
         self.var_load = None
         self.n = None
         
@@ -75,17 +104,20 @@ class HealthScores():
         """
         # data = pd.read_csv(self.read_cols, index_col=0)
         # TODO: clean
-        data = pd.read_csv('data/clean_determinants_std.csv', index_col=0)
+        data = pd.read_csv('data/clean_determinant_std.csv', index_col=0)
         towns = list(data.index)
         return towns
 
-    def calc_pca(self):
+    def calc_pca(self, write=True, dom_data=(None, None)):
         """
         Method to calculate factor scores
         """
-        data = pd.read_csv(self.data, index_col=0)
+        data = None
+        if dom_data[0]:
+            data = dom_data[1]
+        else:
+            data = pd.read_csv(self.data, index_col=0)
         determinant_data = np.array(data)
-
         #find the number of variables
         num_var = determinant_data.shape[1]
 
@@ -121,15 +153,24 @@ class HealthScores():
         
         scores = [round(x,2) for x in health_status]
         towns = self.extract_towns()
-        
-        #assign health scores
-        health_scores = sorted(zip(towns, scores), key = lambda x: x[1])
-        print(health_scores)
 
-        with open(self.output, "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerows(health_scores)
-        
+        if(write == True):
+            #assign health scores
+            health_scores = sorted(zip(towns, scores), key = lambda x: x[1])
+            print(health_scores)
+
+            with open(self.output, "w", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerows(health_scores)
+        return scores 
+
+    def load_data(self):
+        data = pd.read_csv(self.data, index_col=0)
+        return np.array(data)
+    
+    def calc_corr_mat(self, A):
+        return np.cov(A.T)
+
     def calc_loadings(self):
         
         #print table of indicator variable loadings
@@ -142,12 +183,87 @@ class HealthScores():
         with open(self.loadings_file, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerows(loadings)
+    
+    def score_per_domain(self):
+        columns = self.extract_features()
+        domains_by_no = {}
+        domains = self.domains
+
+        for d in domains:
+            for indi in domains[d]:
+                try:
+                    domains_by_no[d].append(str(columns.index(indi)))
+                except KeyError:
+                    domains_by_no[d] = [str(columns.index(indi))]
+
+        determinant_data = pd.read_csv(self.data, index_col=0)
+
+        domain_scores = []
+        for dom in domains_by_no:
+            domain_data = determinant_data[domains_by_no[dom]]
+            domain_scores.append(self.calc_pca(write=False, dom_data=(True, domain_data)))
+
+        domain_scores = np.array(domain_scores).T
+        avg_dom = np.array([[round(x,2) for x in np.average(domain_scores, axis =1)]]).T
+        domain_scores = np.concatenate((domain_scores, avg_dom), axis = 1)
+        towns = self.extract_towns()
+        dom_names = ['index','BE', 'CC', 'ECON', 'EDU', 'EMP', 'HEA', 'HOU', 'VIO', 'AVG']
+        towns = np.array([towns]).T
+        domain_scores = np.concatenate((towns, domain_scores), axis = 1)
+
+        df = pd.DataFrame(data = domain_scores, columns = dom_names)
+        df.set_index('index', inplace=True)
+
+        df.to_csv(self.domain_file)
+    
+    def write_corr_mat(self):
+        A = self.calc_corr_mat(self.load_data())
+        feat = np.array([self.extract_features()]).T
+        A = np.concatenate((feat, A), axis = 1)
+        headers = ["\\"] + self.extract_features()
+        
+        df = pd.DataFrame(data = A, columns = headers)
+        df.set_index("\\", inplace = True)
+        
+        df.to_csv(self.corrmat_file)
 
 def main():
-    health_obj = HealthScores(cols_filepath="data/all_data_std.csv", data_filepath="data/all_data_std.csv",\
-        pca_filepath = "output/pca_all_std.csv", loadings_filepath="output/loadings_all_std.csv")
-    health_obj.calc_pca()
+    
+    health_obj = HealthScores(cols_filepath="data/clean_determinant_std.csv", data_filepath="data/determinant_data_std.csv",\
+       pca_filepath = "output/pca_determinant_std.csv", loadings_filepath="output/loadings_determinant_std.csv", domain_filepath="output/pca_domains_determinant_std.csv",\
+           corrmat_filepath = "output/correlation_matrix_determinant_std.csv")
+    health_obj.calc_pca(write=True)
     health_obj.calc_loadings()
+    health_obj.score_per_domain()
+    health_obj.write_corr_mat()
 
+    health_obj = HealthScores(cols_filepath="data/clean_determinant_mn.csv", data_filepath="data/determinant_data_mn.csv",\
+       pca_filepath = "output/pca_determinant_mn.csv", loadings_filepath="output/loadings_determinant_mn.csv", domain_filepath="output/pca_domains_determinant_mn.csv")
+    health_obj.calc_pca(write=True)
+    health_obj.calc_loadings()
+    health_obj.score_per_domain()
+    
+    health_obj = HealthScores(cols_filepath="data/health_outcomes_std.csv", data_filepath="data/outcome_data_std.csv",\
+       pca_filepath = "output/pca_outcome_std.csv", loadings_filepath="output/loadings_outcome_std.csv", corrmat_filepath = "output/correlation_matrix_outcome_std.csv")
+    health_obj.calc_pca(write=True)
+    health_obj.calc_loadings()
+    health_obj.write_corr_mat()
+    
+    health_obj = HealthScores(cols_filepath="data/health_outcomes_mn.csv", data_filepath="data/outcome_data_mn.csv",\
+       pca_filepath = "output/pca_outcome_mn.csv", loadings_filepath="output/loadings_outcome_mn.csv")
+    health_obj.calc_pca(write=True)
+    health_obj.calc_loadings()    
+    
+    health_obj = HealthScores(cols_filepath="data/all_data_std.csv", data_filepath="data/all_data_std.csv",\
+       pca_filepath = "output/pca_all_std.csv", loadings_filepath="output/loadings_all_std.csv", corrmat_filepath = "output/correlation_matrix_all_std.csv.csv")
+    health_obj.calc_pca(write=True)
+    health_obj.calc_loadings()
+    health_obj.write_corr_mat()
+
+    health_obj = HealthScores(cols_filepath="data/all_data_mn.csv", data_filepath="data/all_data_mn.csv",\
+       pca_filepath = "output/pca_all_mn.csv", loadings_filepath="output/loadings_all_mn.csv")
+    health_obj.calc_pca(write=True)
+    health_obj.calc_loadings()
+    
 if __name__ == '__main__':
     main()
